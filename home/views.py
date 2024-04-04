41d16b757791e4123f79445ee6bcea2f5f5e8036
WashCout <washington5.coutinho@gmail.com>
2024-04-04 01:05:33 -0300

    Atualizações de login de acesso

--- home/views.py
+++ home/views.py
@@ -2,24 +2,26 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+
 
 
 def ver_home(request):
-    nome = 'Wash'
-    return render(request,'ver_home.html', {'nome':nome})
-
-def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('nome_da_url_para_pagina_de_pedidos')
-        else:
-            return render(request, 'home/login.html', {'form': form})
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                
+                return HttpResponse('você foi logado')
     else:
         form = AuthenticationForm()
-    return render(request, 'home/login.html', {'form': form})
+    return render(request, 'ver_home.html', {'form': form})
+
+def login(request):
+    return HttpResponse('Aqui você pode realizar seus pedidos')
 
 def inserir_home(request):
     return HttpResponse('Aqui você pode realizar seus pedidos')
