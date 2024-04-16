f0af86236bc9f654926f0202580531447e539411
gugatwos <gugatwos@gmail.com>
2024-04-16 04:48:36 -0300

    database

--- lojistas/admin.py
+++ lojistas/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Produto, Lojista, Estoque, Vendas
 
-# Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor', 'quantidade')
+    search_fields = ('descricao',)
+
+class LojistaAdmin(admin.ModelAdmin):
+    list_display = ('user', 'endereco', 'telefone', 'email')
+    search_fields = ('user__username', 'endereco')
+
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = ('produto', 'quantidade')
+    search_fields = ('produto__descricao',)
+
+class VendasAdmin(admin.ModelAdmin):
+    list_display = ('processo', 'lojista', 'valor_produto')
+    search_fields = ('lojista__user__username',)
+
+# Registro dos modelos com as classes ModelAdmin
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Lojista, LojistaAdmin)
+admin.site.register(Estoque, EstoqueAdmin)
+admin.site.register(Vendas, VendasAdmin)
--- lojistas/models.py
+++ lojistas/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+class Produto(models.Model):
+    CATEGORIA_CHOICES = [
+        ('POTE', 'Sorvete de Pote'),
+        ('MASSA', 'Sorvete de Massa'),
+        ('PICOLE', 'Picolé'),
+    ]
+    nome = models.CharField(max_length=50, default = 'sorvete')
+    descricao = models.CharField(max_length=300)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    categoria = models.CharField(max_length=15, choices=CATEGORIA_CHOICES, default='POTE')
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.descricao
 
 class Lojista(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nome_da_loja = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=300)
+    telefone = models.CharField(max_length=20)  # Alterado para CharField
+    email = models.CharField(max_length=100)
 
 class Estoque(models.Model):
-    lojista = models.ForeignKey(Lojista, on_delete=models.CASCADE)
-    produto = models.CharField(max_length=255)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='produtos')
     quantidade = models.IntegerField()
+
+class Vendas(models.Model):
+    processo = models.BooleanField()
+    lojista = models.ForeignKey(Lojista, on_delete=models.CASCADE)
+    valor_produto = models.DecimalField(max_digits=8, decimal_places=2)  # Corrigido nome e aumentado max_digits
